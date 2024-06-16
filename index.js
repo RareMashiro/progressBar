f07659641f8ceb86ba458769dd841d0dac6f507b
RareMashiro <symon.belmont2015@yandex.ru>
2024-06-16 08:57:01 +0300

    Almost done. Still need to be improved by content-device dependency

--- index.js
+++ index.js
@@ -1,2 +1,59 @@
 // Начало проекта
-console.log('Preparations complete!');
+
+const control = document.querySelector('.circle')
+const btnOne = document.querySelector('#checkboxOne');
+const btnTwo = document.querySelector('#checkboxTwo');
+let range = document.querySelector('.value')
+
+btnOne.addEventListener('click', animate);
+btnTwo.addEventListener('click', hide);
+range.addEventListener("input", draw);
+
+let flagOne = true;
+let flagTwo = true;
+let start = 0;
+let id = null;
+
+function animate() {
+    if(flagOne) {
+        control.style.animation = 'rotation 2s linear infinite';
+        flagOne = !flagOne;
+    } else {
+        control.style.animation = 'none';
+        flagOne = !flagOne;
+    }
+}
+
+function hide() {
+    if(flagTwo) {
+        control.style.display = 'none';
+        flagTwo = !flagTwo;
+    } else {
+        control.style.display = 'flex';
+        flagTwo = !flagTwo;
+    }
+}
+
+function draw() {
+    if (id) {
+        clearInterval(id);
+    }
+
+    let target = parseInt(range.value);
+    id = setInterval(() => {
+        if (start < target) {
+            start++;
+            end();
+        } else if (start > target) {
+            start--;
+            end();
+        } else {
+            clearInterval(id);
+            id = null;
+        }
+    }, 30);
+
+    function end() {
+        control.style.background = `conic-gradient(blue ${start * 3.6}deg, #ededed ${start * 3.6}deg 0deg)`
+    }
+}
