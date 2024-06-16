c2747b118d267ddeaf8450364e7b461c5ac87b5a
RareMashiro <symon.belmont2015@yandex.ru>
2024-06-16 13:59:46 +0300

    Upload speed was increased,

--- script.js
+++ script.js
@@ -1,60 +1,61 @@
-const control = document.querySelector('.circle')
-const btnOne = document.querySelector('#checkboxOne');
-const btnTwo = document.querySelector('#checkboxTwo');
-let range = document.querySelector('.value')
+const circleComponent = (() => {
+    const control = document.querySelector('.circle');
+    const range = document.querySelector('.value');
 
-btnOne.addEventListener('click', animate);
-btnTwo.addEventListener('click', hide);
-range.addEventListener("input", draw);
+    let animationId = null;
+    let flagOne = true;
+    let flagTwo = true;
+    let start = 0;
 
-let flagOne = true;
-let flagTwo = true;
-let start = 0;
-let id = null;
-
-function animate() {
-    if(flagOne) {
-        control.style.animation = 'rotation 1s linear infinite';
-        flagOne = !flagOne;
-    } else {
-        control.style.animation = 'none';
-        flagOne = !flagOne;
-    }
-}
-
-function hide() {
-    if(flagTwo) {
-        control.style.display = 'none';
-        flagTwo = !flagTwo;
-    } else {
-        control.style.display = 'flex';
-        flagTwo = !flagTwo;
-    }
-}
-
-function draw() {
-    if (id) {
-        clearInterval(id);
-    }
-
-    let target = parseInt(range.value);
-    if(target > 100) target = 100;
-    if(target < 0) target = 0;
-
-    id = setInterval(() => {
-        if (start < target) {
-            start++;
-            end();
-        } else if (start > target) {
-            start--;
-            end();
+    function animate() {
+        if(flagOne) {
+            control.style.animation = 'rotation 1s linear infinite';
         } else {
-            clearInterval(id);
-            id = null;
+            control.style.animation = 'none';
         }
-    }, 30);
-
-    function end() {
-        control.style.background = `conic-gradient(blue ${start * 3.6}deg, #ededed ${start * 3.6}deg 0deg)`
+        flagOne = !flagOne;
     }
-}
+
+    function hide() {
+        if(flagTwo) {
+            control.style.display = 'none';
+        } else {
+            control.style.display = 'flex';
+        }
+        flagTwo = !flagTwo;
+    }
+
+    function draw() {
+        if (animationId) {
+            clearInterval(animationId);
+        }
+
+        let target = parseInt(range.value);
+        if(target > 100) target = 100;
+        if(target < 0) target = 0;
+
+        animationId = setInterval(() => {
+            if (start < target) {
+                start++;
+                end();
+            } else if (start > target) {
+                start--;
+                end();
+            } else {
+                clearInterval(animationId);
+                animationId = null;
+            }
+        }, 10);
+
+        function end() {
+            // rotatePosition = start * 3.6;
+            control.style.background = `conic-gradient(blue ${start * 3.6}deg, #ededed ${start * 3.6}deg 0deg)`
+        }
+    }
+
+    return { animate, draw, hide };
+})();
+
+document.querySelector('#checkboxOne').addEventListener('click', () => circleComponent.animate());
+document.querySelector('#checkboxTwo').addEventListener('click', () => circleComponent.hide());
+document.querySelector('.value').addEventListener('input', circleComponent.draw);
